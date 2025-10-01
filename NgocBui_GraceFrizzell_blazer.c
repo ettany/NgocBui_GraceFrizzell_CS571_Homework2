e49a2983428f543837b4c06fb0b78e44a4bd79d2
ettany <67234683+ettany@users.noreply.github.com>
2025-10-01 19:34:07 -0400

    update with lex

--- NgocBui_GraceFrizzell_blazer.c
+++ NgocBui_GraceFrizzell_blazer.c
@@ -1,311 +1,356 @@
-/*
- * NgocBui_GraceFrizzell_blazer.c
- * Blazer Language Scanner and Parser
- * Implements lexical analysis and recursive descent parsing
- * for a simple language with arithmetic expressions and I/O
- * 
- * Authors: Ngoc Bui and Grace Frizzell
+/* blazer.c - A scanner and parser for the Blazer language
+ * Context-Free Grammar:
+ * <program> -> <stmt>{<stmt>}'halt'
+ * <stmt>    -> T_ID '=' <expr> | 'read' T_ID | 'write' <expr>
+ * <expr>    -> <term> {('+' | '-') <term>}
+ * <term>    -> <factor> {('*' | '/') <factor>}
+ * <factor>  -> T_ID | T_INT_CONSTANT | '(' <expr> ')'
  */
 
 #include <stdio.h>
+#include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
 
-/* Token types */
-typedef enum {
-    T_ID,
-    T_INT_CONSTANT,
-    T_ASSIGN,      // =
-    T_PLUS,        // +
-    T_MINUS,       // -
-    T_TIMES,       // *
-    T_DIV,         // /
-    T_LPAREN,      // (
-    T_RPAREN,      // )
-    T_READ,        // read
-    T_WRITE,       // write
-    T_HALT,        // halt
-    T_EOF,
-    T_ERROR
-} TokenType;
+/* Global declarations */
+int charClass;
+char lexeme[100];
+char nextChar;
+int lexLen;
+int token;
+int nextToken;
+FILE *in_fp;
+int errorCount = 0;
+int haltFound = 0;
 
-/* Token structure */
-typedef struct {
-    TokenType type;
-    char lexeme[256];
-    int line_num;
-} Token;
-
-/* Global variables */
-FILE *source_file;
-Token current_token;
-int line_number = 1;
-char current_char;
-
-/* Function prototypes */
-void get_char();
-void skip_whitespace();
-Token get_next_token();
-void match(TokenType expected);
-void error(const char *message);
-
-/* Parser function prototypes */
+/* Function declarations */
+void addChar();
+void getChar();
+void getNonBlank();
+int lex();
 void program();
-void stmt();
+void statement();
 void expr();
 void term();
 void factor();
+void error(const char* msg);
 
-/* Scanner Implementation */
+/* Character classes */
+#define LETTER 0
+#define DIGIT 1
+#define UNKNOWN 99
 
-void get_char() {
-    current_char = fgetc(source_file);
-    if (current_char == '\n') {
-        line_number++;
-    }
-}
+/* Token codes */
+#define INT_LIT 10
+#define IDENT 11
+#define ASSIGN_OP 20
+#define ADD_OP 21
+#define SUB_OP 22
+#define MULT_OP 23
+#define DIV_OP 24
+#define LEFT_PAREN 25
+#define RIGHT_PAREN 26
+#define READ_KW 27
+#define WRITE_KW 28
+#define HALT_KW 29
 
-void skip_whitespace() {
-    while (isspace(current_char)) {
-        get_char();
-    }
-}
-
-Token get_next_token() {
-    Token token;
-    token.line_num = line_number;
-    
-    skip_whitespace();
-    
-    /* Check for EOF */
-    if (current_char == EOF) {
-        token.type = T_EOF;
-        strcpy(token.lexeme, "EOF");
-        return token;
-    }
-    
-    /* Check for identifiers and keywords */
-    if (isalpha(current_char)) {
-        int i = 0;
-        while (isalnum(current_char) && i < 255) {
-            token.lexeme[i++] = current_char;
-            get_char();
-        }
-        token.lexeme[i] = '\0';
-        
-        /* Check for keywords */
-        if (strcmp(token.lexeme, "read") == 0) {
-            token.type = T_READ;
-        } else if (strcmp(token.lexeme, "write") == 0) {
-            token.type = T_WRITE;
-        } else if (strcmp(token.lexeme, "halt") == 0) {
-            token.type = T_HALT;
-        } else {
-            token.type = T_ID;
-        }
-        return token;
-    }
-    
-    /* Check for integer constants */
-    if (isdigit(current_char)) {
-        int i = 0;
-        while (isdigit(current_char) && i < 255) {
-            token.lexeme[i++] = current_char;
-            get_char();
-        }
-        token.lexeme[i] = '\0';
-        token.type = T_INT_CONSTANT;
-        return token;
-    }
-    
-    /* Check for operators and delimiters */
-    switch (current_char) {
-        case '=':
-            token.type = T_ASSIGN;
-            token.lexeme[0] = current_char;
-            token.lexeme[1] = '\0';
-            get_char();
-            break;
-        case '+':
-            token.type = T_PLUS;
-            token.lexeme[0] = current_char;
-            token.lexeme[1] = '\0';
-            get_char();
-            break;
-        case '-':
-            token.type = T_MINUS;
-            token.lexeme[0] = current_char;
-            token.lexeme[1] = '\0';
-            get_char();
-            break;
-        case '*':
-            token.type = T_TIMES;
-            token.lexeme[0] = current_char;
-            token.lexeme[1] = '\0';
-            get_char();
-            break;
-        case '/':
-            token.type = T_DIV;
-            token.lexeme[0] = current_char;
-            token.lexeme[1] = '\0';
-            get_char();
-            break;
-        case '(':
-            token.type = T_LPAREN;
-            token.lexeme[0] = current_char;
-            token.lexeme[1] = '\0';
-            get_char();
-            break;
-        case ')':
-            token.type = T_RPAREN;
-            token.lexeme[0] = current_char;
-            token.lexeme[1] = '\0';
-            get_char();
-            break;
-        default:
-            token.type = T_ERROR;
-            sprintf(token.lexeme, "Invalid character: '%c'", current_char);
-            get_char();
-            break;
-    }
-    
-    return token;
-}
-
-/* Parser Implementation */
-
-void error(const char *message) {
-    fprintf(stderr, "Error at line %d: %s\n", current_token.line_num, message);
-    fprintf(stderr, "Current token: %s\n", current_token.lexeme);
-    exit(1);
-}
-
-void match(TokenType expected) {
-    if (current_token.type == expected) {
-        printf("Matched: %s\n", current_token.lexeme);
-        current_token = get_next_token();
-    } else {
-        char error_msg[256];
-        sprintf(error_msg, "Expected token type %d but found %s", 
-                expected, current_token.lexeme);
-        error(error_msg);
-    }
-}
-
-/* <program> -> <stmt>{<stmt>}'halt' */
-void program() {
-    printf("Parsing program...\n");
-    
-    while (current_token.type != T_HALT && current_token.type != T_EOF) {
-        stmt();
-    }
-    
-    if (current_token.type == T_HALT) {
-        match(T_HALT);
-        printf("Program parsed successfully!\n");
-    } else {
-        error("Expected 'halt' at end of program");
-    }
-}
-
-/* <stmt> -> T_ID '=' <expr>
-          | 'read' T_ID
-          | 'write' <expr> */
-void stmt() {
-    printf("Parsing statement...\n");
-    
-    if (current_token.type == T_ID) {
-        match(T_ID);
-        match(T_ASSIGN);
-        expr();
-    } else if (current_token.type == T_READ) {
-        match(T_READ);
-        match(T_ID);
-    } else if (current_token.type == T_WRITE) {
-        match(T_WRITE);
-        expr();
-    } else {
-        error("Expected statement (assignment, read, or write)");
-    }
-}
-
-/* <expr> -> <term> {('+'|'-')<term>} */
-void expr() {
-    printf("Parsing expression...\n");
-    
-    term();
-    
-    while (current_token.type == T_PLUS || current_token.type == T_MINUS) {
-        if (current_token.type == T_PLUS) {
-            match(T_PLUS);
-        } else {
-            match(T_MINUS);
-        }
-        term();
-    }
-}
-
-/* <term> -> <factor> {('*'|'/') <factor>} */
-void term() {
-    printf("Parsing term...\n");
-    
-    factor();
-    
-    while (current_token.type == T_TIMES || current_token.type == T_DIV) {
-        if (current_token.type == T_TIMES) {
-            match(T_TIMES);
-        } else {
-            match(T_DIV);
-        }
-        factor();
-    }
-}
-
-/* <factor> -> T_ID
-            | T_INT_CONSTANT
-            | '('<expr>')' */
-void factor() {
-    printf("Parsing factor...\n");
-    
-    if (current_token.type == T_ID) {
-        match(T_ID);
-    } else if (current_token.type == T_INT_CONSTANT) {
-        match(T_INT_CONSTANT);
-    } else if (current_token.type == T_LPAREN) {
-        match(T_LPAREN);
-        expr();
-        match(T_RPAREN);
-    } else {
-        error("Expected identifier, constant, or '(' in factor");
-    }
-}
-
-/* Main function */
+/******************************************************/
+/* main driver */
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <source_file.bl>\n", argv[0]);
-        return 1;
+    char filename[100];
+    
+    /* Determine input file */
+    if (argc > 1) {
+        strcpy(filename, argv[1]);
+    } else {
+        strcpy(filename, "front.in");
     }
     
-    source_file = fopen(argv[1], "r");
-    if (source_file == NULL) {
-        fprintf(stderr, "Error: Could not open file '%s'\n", argv[1]);
-        return 1;
+    /* Open the input data file */
+    if ((in_fp = fopen(filename, "r")) == NULL) {
+        printf("ERROR - cannot open %s\n", filename);
+        exit(1);
     }
     
-    printf("Blazer Language Parser\n");
-    printf("======================\n");
-    printf("Parsing file: %s\n\n", argv[1]);
+    printf("\nstarting the parsing process...\n");
     
-    /* Initialize scanner */
-    get_char();
-    current_token = get_next_token();
+    /* Initialize lexer */
+    getChar();
+    lex();
     
     /* Start parsing */
     program();
     
-    fclose(source_file);
+    /* Close file */
+    fclose(in_fp);
+    
+    /* Report results */
+    if (errorCount > 0) {
+        printf("\n--- Found %d error(s) ---\n", errorCount);
+        printf("This program is invalid.\n");
+    } else {
+        printf("This is a valid program.\n");
+    }
     
     return 0;
+}
+
+/*****************************************************/
+/* error - handle parsing errors */
+void error(const char* msg) {
+    errorCount++;
+    printf("%d: %s\n", errorCount, msg);
+}
+
+/*****************************************************/
+/* lookup - lookup operators and parentheses */
+int lookup(char ch) {
+    switch (ch) {
+        case '(':
+            addChar();
+            nextToken = LEFT_PAREN;
+            break;
+        case ')':
+            addChar();
+            nextToken = RIGHT_PAREN;
+            break;
+        case '+':
+            addChar();
+            nextToken = ADD_OP;
+            break;
+        case '-':
+            addChar();
+            nextToken = SUB_OP;
+            break;
+        case '*':
+            addChar();
+            nextToken = MULT_OP;
+            break;
+        case '/':
+            addChar();
+            nextToken = DIV_OP;
+            break;
+        case '=':
+            addChar();
+            nextToken = ASSIGN_OP;
+            break;
+        default:
+            addChar();
+            nextToken = EOF;
+            break;
+    }
+    return nextToken;
+}
+
+/*****************************************************/
+/* addChar - add nextChar to lexeme */
+void addChar() {
+    if (lexLen <= 98) {
+        lexeme[lexLen++] = nextChar;
+        lexeme[lexLen] = 0;
+    } else {
+        printf("Error - lexeme is too long\n");
+    }
+}
+
+/*****************************************************/
+/* getChar - get next character and determine its class */
+void getChar() {
+    if ((nextChar = getc(in_fp)) != EOF) {
+        if (isalpha(nextChar))
+            charClass = LETTER;
+        else if (isdigit(nextChar))
+            charClass = DIGIT;
+        else
+            charClass = UNKNOWN;
+    } else {
+        charClass = EOF;
+    }
+}
+
+/*****************************************************/
+/* getNonBlank - skip whitespace */
+void getNonBlank() {
+    while (isspace(nextChar))
+        getChar();
+}
+
+/*****************************************************/
+/* lex - lexical analyzer */
+int lex() {
+    lexLen = 0;
+    getNonBlank();
+    
+    switch (charClass) {
+        /* Identifiers and Keywords */
+        case LETTER:
+            addChar();
+            getChar();
+            while (charClass == LETTER || charClass == DIGIT) {
+                addChar();
+                getChar();
+            }
+            
+            /* Check for keywords */
+            if (strcmp(lexeme, "read") == 0)
+                nextToken = READ_KW;
+            else if (strcmp(lexeme, "write") == 0)
+                nextToken = WRITE_KW;
+            else if (strcmp(lexeme, "halt") == 0)
+                nextToken = HALT_KW;
+            else
+                nextToken = IDENT;
+            break;
+            
+        /* Integer literals */
+        case DIGIT:
+            addChar();
+            getChar();
+            while (charClass == DIGIT) {
+                addChar();
+                getChar();
+            }
+            nextToken = INT_LIT;
+            break;
+            
+        /* Operators and parentheses */
+        case UNKNOWN:
+            lookup(nextChar);
+            getChar();
+            break;
+            
+        /* EOF */
+        case EOF:
+            nextToken = EOF;
+            lexeme[0] = 'E';
+            lexeme[1] = 'O';
+            lexeme[2] = 'F';
+            lexeme[3] = 0;
+            break;
+    }
+    
+    printf("Call lex...%s\n", lexeme);
+    return nextToken;
+}
+
+/*****************************************************/
+/* program - parse <program> -> <stmt>{<stmt>}'halt' */
+void program() {
+    printf("Enter <program>\n");
+    
+    /* Parse statements until 'halt' or EOF */
+    while (nextToken != HALT_KW && nextToken != EOF) {
+        statement();
+    }
+    
+    /* Check for 'halt' keyword */
+    if (nextToken == HALT_KW) {
+        haltFound = 1;
+        lex();
+        
+        /* Check for statements after 'halt' */
+        if (nextToken != EOF) {
+            error("ERROR: STATEMENTS AFTER 'halt'.");
+        }
+    } else {
+        error("ERROR: NO 'halt' STATEMENT.");
+    }
+    
+    printf("Exit <program>\n");
+}
+
+/*****************************************************/
+/* statement - parse <stmt> */
+void statement() {
+    printf("Enter <statement>\n");
+    
+    if (nextToken == READ_KW) {
+        /* 'read' T_ID */
+        lex();
+        if (nextToken == IDENT) {
+            lex();
+        } else {
+            error("Invalid read statement.");
+        }
+    }
+    else if (nextToken == WRITE_KW) {
+        /* 'write' <expr> */
+        lex();
+        expr();
+    }
+    else if (nextToken == IDENT) {
+        /* T_ID '=' <expr> */
+        lex();
+        if (nextToken == ASSIGN_OP) {
+            lex();
+            expr();
+        } else {
+            error("Invalid assignment statement.");
+        }
+    }
+    else {
+        /* Unexpected token */
+        error("Invalid statement.");
+        lex();  /* Skip the invalid token */
+    }
+    
+    printf("Exit <statement>\n");
+}
+
+/*****************************************************/
+/* expr - parse <expr> -> <term> {('+' | '-') <term>} */
+void expr() {
+    printf("Enter <expr>\n");
+    
+    term();
+    
+    while (nextToken == ADD_OP || nextToken == SUB_OP) {
+        lex();
+        term();
+    }
+    
+    printf("Exit <expr>\n");
+}
+
+/*****************************************************/
+/* term - parse <term> -> <factor> {('*' | '/') <factor>} */
+void term() {
+    printf("Enter <term>\n");
+    
+    factor();
+    
+    while (nextToken == MULT_OP || nextToken == DIV_OP) {
+        lex();
+        factor();
+    }
+    
+    printf("Exit <term>\n");
+}
+
+/*****************************************************/
+/* factor - parse <factor> -> T_ID | T_INT_CONSTANT | '(' <expr> ')' */
+void factor() {
+    printf("Enter <factor>\n");
+    
+    if (nextToken == IDENT || nextToken == INT_LIT) {
+        /* T_ID or T_INT_CONSTANT */
+        lex();
+    }
+    else if (nextToken == LEFT_PAREN) {
+        /* '(' <expr> ')' */
+        lex();
+        expr();
+        if (nextToken == RIGHT_PAREN) {
+            lex();
+        } else {
+            error("Missing closing parenthesis in factor.");
+        }
+    }
+    else {
+        /* Invalid token in factor */
+        error("Invalid token in factor.");
+        lex();
+    }
+    
+    printf("Exit <factor>\n");
 }
